920ec0f32b41576f06893e7a20b95c7a4ed1a67e
Jin Lee <leepc12@gmail.com>
2021-06-07 11:55:03 -0700

    add shell script to transfer artifacts/templates to s3://caper-aws-genomics-workflows

--- caper/sync_dist_to_caper_data.sh
+++ caper/sync_dist_to_caper_data.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-LAST_COMMIT_HASH=$(git rev-list HEAD | head -n1)
-touch "../dist/last_git_commit_${LAST_COMMIT_HASH}"
-
+git rev-list HEAD | head -n10 > ../dist/last_10_git_commit
 aws s3 sync ../dist/ s3://caper-aws-genomics-workflows
 
